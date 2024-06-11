39f1ea9ff08f0a5d9ecb0050fc0661e1c134f0dc
Ronak Surve <ronaksurve344rs@gmail.com>
2024-06-11 20:15:15 +0530

    session id and cookie added

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const userModel = require("../models/userModel");
+const {setUser, getUser} = require("../service/authService");
+const { v4: uuidv4 } = require('uuid');
 
 async function handleUserSignUp(req,res)    {
 
     const body = req.body;
+
+    if(!body.name || !body.email || !body.password) {
+        return res.render("signup", {
+            message : "All fields are required",
+        });
+    }
 
     await userModel.create({
         name : body.name,
@@ -12,7 +20,31 @@ async function handleUserSignUp(req,res)    {
         password : body.password,
     });
 
-    return res.render("home");
+    return res.redirect("/");
 }
 
-module.exports = {handleUserSignUp};
+async function handleUserLogin(req,res) {
+
+    const body = req.body;
+
+    const user = await userModel.findOne({
+        email : body.email,
+        password : body.password,
+    });
+
+    if(!user)   {
+        return res.render("login",  {
+            error : "Invalid username or password",
+        })
+    }
+    
+    const sessionId = uuidv4();
+
+    setUser(sessionId,user);
+
+    res.cookie("uid", sessionId);
+
+    return res.redirect("/");
+}
+
+module.exports = {handleUserSignUp, handleUserLogin};
